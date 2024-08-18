f41953d4dfd8fbb8a13052c85cddf84472b97059
Krishna Patel <121641611+krishnap21@users.noreply.github.com>
2024-08-17 22:41:03 -0600

    Add files via upload

---
+++ Base_Converter/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Base_Converter
+{
+    partial class BaseConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DecimalBtn = new System.Windows.Forms.Button();
+            this.BinaryBtn = new System.Windows.Forms.Button();
+            this.HexadecimalBtn = new System.Windows.Forms.Button();
+            this.DecimalTxt = new System.Windows.Forms.TextBox();
+            this.BinaryTxt = new System.Windows.Forms.TextBox();
+            this.HexadecimalTxt = new System.Windows.Forms.TextBox();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.DecimalLbl = new System.Windows.Forms.Label();
+            this.BinaryLbl = new System.Windows.Forms.Label();
+            this.HexadecimalLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DecimalBtn
+            // 
+            this.DecimalBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecimalBtn.Location = new System.Drawing.Point(584, 146);
+            this.DecimalBtn.Name = "DecimalBtn";
+            this.DecimalBtn.Size = new System.Drawing.Size(204, 57);
+            this.DecimalBtn.TabIndex = 0;
+            this.DecimalBtn.Text = "Convert";
+            this.DecimalBtn.UseVisualStyleBackColor = true;
+            this.DecimalBtn.Click += new System.EventHandler(this.DecimalBtn_Click);
+            // 
+            // BinaryBtn
+            // 
+            this.BinaryBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryBtn.Location = new System.Drawing.Point(584, 246);
+            this.BinaryBtn.Name = "BinaryBtn";
+            this.BinaryBtn.Size = new System.Drawing.Size(204, 57);
+            this.BinaryBtn.TabIndex = 0;
+            this.BinaryBtn.Text = "Convert";
+            this.BinaryBtn.UseVisualStyleBackColor = true;
+            this.BinaryBtn.Click += new System.EventHandler(this.BinaryBtn_Click);
+            // 
+            // HexadecimalBtn
+            // 
+            this.HexadecimalBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HexadecimalBtn.Location = new System.Drawing.Point(584, 346);
+            this.HexadecimalBtn.Name = "HexadecimalBtn";
+            this.HexadecimalBtn.Size = new System.Drawing.Size(204, 57);
+            this.HexadecimalBtn.TabIndex = 0;
+            this.HexadecimalBtn.Text = "Convert";
+            this.HexadecimalBtn.UseVisualStyleBackColor = true;
+            this.HexadecimalBtn.Click += new System.EventHandler(this.HexadecimalBtn_Click);
+            // 
+            // DecimalTxt
+            // 
+            this.DecimalTxt.Location = new System.Drawing.Point(243, 146);
+            this.DecimalTxt.Multiline = true;
+            this.DecimalTxt.Name = "DecimalTxt";
+            this.DecimalTxt.Size = new System.Drawing.Size(291, 57);
+            this.DecimalTxt.TabIndex = 3;
+            this.DecimalTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BinaryTxt
+            // 
+            this.BinaryTxt.Location = new System.Drawing.Point(243, 246);
+            this.BinaryTxt.Multiline = true;
+            this.BinaryTxt.Name = "BinaryTxt";
+            this.BinaryTxt.Size = new System.Drawing.Size(291, 57);
+            this.BinaryTxt.TabIndex = 3;
+            this.BinaryTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // HexadecimalTxt
+            // 
+            this.HexadecimalTxt.Location = new System.Drawing.Point(243, 346);
+            this.HexadecimalTxt.Multiline = true;
+            this.HexadecimalTxt.Name = "HexadecimalTxt";
+            this.HexadecimalTxt.Size = new System.Drawing.Size(291, 57);
+            this.HexadecimalTxt.TabIndex = 3;
+            this.HexadecimalTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLbl.Location = new System.Drawing.Point(11, 9);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(777, 73);
+            this.TitleLbl.TabIndex = 6;
+            this.TitleLbl.Text = "BASE CONVERTER";
+            // 
+            // DecimalLbl
+            // 
+            this.DecimalLbl.AutoSize = true;
+            this.DecimalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecimalLbl.Location = new System.Drawing.Point(29, 159);
+            this.DecimalLbl.Name = "DecimalLbl";
+            this.DecimalLbl.Size = new System.Drawing.Size(149, 37);
+            this.DecimalLbl.TabIndex = 7;
+            this.DecimalLbl.Text = "Decimal:";
+            // 
+            // BinaryLbl
+            // 
+            this.BinaryLbl.AutoSize = true;
+            this.BinaryLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryLbl.Location = new System.Drawing.Point(38, 259);
+            this.BinaryLbl.Name = "BinaryLbl";
+            this.BinaryLbl.Size = new System.Drawing.Size(123, 37);
+            this.BinaryLbl.TabIndex = 8;
+            this.BinaryLbl.Text = "Binary:";
+            // 
+            // HexadecimalLbl
+            // 
+            this.HexadecimalLbl.AutoSize = true;
+            this.HexadecimalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HexadecimalLbl.Location = new System.Drawing.Point(0, 359);
+            this.HexadecimalLbl.Name = "HexadecimalLbl";
+            this.HexadecimalLbl.Size = new System.Drawing.Size(221, 37);
+            this.HexadecimalLbl.TabIndex = 9;
+            this.HexadecimalLbl.Text = "Hexadecimal:";
+            // 
+            // BaseConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HexadecimalLbl);
+            this.Controls.Add(this.BinaryLbl);
+            this.Controls.Add(this.DecimalLbl);
+            this.Controls.Add(this.TitleLbl);
+            this.Controls.Add(this.HexadecimalTxt);
+            this.Controls.Add(this.BinaryTxt);
+            this.Controls.Add(this.DecimalTxt);
+            this.Controls.Add(this.HexadecimalBtn);
+            this.Controls.Add(this.BinaryBtn);
+            this.Controls.Add(this.DecimalBtn);
+            this.Name = "BaseConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Base Converter:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DecimalBtn;
+        private System.Windows.Forms.Button BinaryBtn;
+        private System.Windows.Forms.Button HexadecimalBtn;
+        private System.Windows.Forms.TextBox DecimalTxt;
+        private System.Windows.Forms.TextBox BinaryTxt;
+        private System.Windows.Forms.TextBox HexadecimalTxt;
+        private System.Windows.Forms.Label TitleLbl;
+        private System.Windows.Forms.Label DecimalLbl;
+        private System.Windows.Forms.Label BinaryLbl;
+        private System.Windows.Forms.Label HexadecimalLbl;
+    }
+}
+
---
+++ Base_Converter/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base_Converter
+{
+    public partial class BaseConverter : Form
+    {
+        // BaseConverter - Initialize the screen
+        public BaseConverter()
+        {
+            InitializeComponent();
+        } // BaseConverter
+
+
+        // DecimalBtn_Click - If the covert button for the decimal text is clicked, the program checks if the user inputted a decimal number;
+        //                    if so, the number is converted to the other bases. If not, the program tells the user to enter a decimal number
+        private void DecimalBtn_Click(object sender, EventArgs e)
+        {
+            int decimalnum; // The decimal number that the user inputted
+            bool valid;   // Used to make sure that the user entered a decimal number
+
+            // Initialize to false
+            valid = false;
+
+            // Make sure that the number that the user inputed is actually a decimal number
+            if(int.TryParse(DecimalTxt.Text, out decimalnum))
+            {
+                // The user did enter a decimal number so set to true
+                valid = true;
+            } // if
+
+            // If the number that the user inputted is decimal convert it to the other bases
+            if (valid)
+            {
+                // Update the binary text to show the binary equivalent to the decimal number that the user inputted
+                BinaryTxt.Text = Convert.ToString(decimalnum, 2);
+                // Update the hexadecimal text to show the hexadecimal equivalent to the decimal number that the user inputted
+                HexadecimalTxt.Text = Convert.ToString(decimalnum, 16);
+            } // if
+            else
+            {
+                // The user didn't enter a decimal number so tell them to enter one
+                MessageBox.Show("Invalid - please enter a decimal value in this box");
+
+                // Clear all of the text boxes
+                DecimalTxt.Clear();
+                BinaryTxt.Clear();
+                HexadecimalTxt.Clear();
+            } // else
+
+        } // DecimalBtn_Click
+
+        // BinaryBtn_Click - If the covert button for the binary text is clicked, the program checks if the user inputted a binary number;
+        //                    if so, the number is converted to the other bases. If not, the program tells the user to enter a binary number
+        private void BinaryBtn_Click(object sender, EventArgs e)
+        {
+            bool valid;         // Used to make sure that the user entered a binary number
+            string binarynum;   // The binary number that the user inputted
+            int temp;           // Temporary place holder for a decimal equivalent of binary
+
+            // Initialize to false
+            valid = false;
+            // Make the binary number identifer equal the number that the user inputted in the text box
+            binarynum = BinaryTxt.Text;
+
+            // If the number that the user inputted is a binary number that consists of only 0's and 1's
+            if (Regex.IsMatch(binarynum, "^[01]+$"))
+            {
+                // The user entered a valid binary number so set identifier to true
+                valid = true;
+            } // if
+
+            // If the number that the user inputted is binary convert it to the other bases
+            if (valid)
+            {
+                // Update the decimal text to show the decimal equivalent to the binary number that the user inputted
+                DecimalTxt.Text = Convert.ToInt32(BinaryTxt.Text, 2).ToString();
+
+                // Convert the binary number to decimal number and assign it to the temp identifier
+                temp = Convert.ToInt32(BinaryTxt.Text, 2);
+
+                // Convert the decimal number that is assigned to the temp identifier to hexadecimal and update the hexidecimal text to display
+                // the hexadecimal equivalent to the binary number that the user inputted
+                HexadecimalTxt.Text = Convert.ToString(temp, 16);
+            } // if
+            else
+            {
+                // The user didn't enter a binary number so tell them to enter one
+                MessageBox.Show("Invalid - please enter a binary value in this box");
+
+                // Clear all of the text boxes
+                DecimalTxt.Clear();
+                BinaryTxt.Clear();
+                HexadecimalTxt.Clear();
+
+            } // else
+
+        } // BinaryBtn_Click
+
+
+        // HexadecimalBtn_Click - If the covert button for the hexadecimal text is clicked, the program checks if the user inputted a
+        //                        hexadecimal number; if so, the number is converted to the other bases. If not, the program tells the
+        //                        user to enter a hexadecimal number
+        private void HexadecimalBtn_Click(object sender, EventArgs e)
+        {
+            bool valid;              // Used to make sure that the user entered a binary number
+            string hexadecimalnum;   // The hexadecimal number that the user inputted
+            int temp;                // Temporary place holder for decimal equivalent of hexadecimal
+
+            // Initialize to false
+            valid = false;
+            // Make the hexadecimal number identifer equal the number that the user inputted in the text box
+            hexadecimalnum = HexadecimalTxt.Text;
+
+            // If the number that the user inputted is a hexadecimal number that consists of only 0-9 and a-f
+            if (Regex.IsMatch(hexadecimalnum, "^[a-fA-F0-9]+$"))
+            {
+                // The user entered a valid hexadecimal number so set identifier to true
+                valid = true;
+            } // if
+
+            // If the number that the user inputted is hexadecimal convert it to the other bases
+            if (valid)
+            {
+                // Update the decimal text to show the decimal equivalent to the hexadecimal number that the user inputted
+                DecimalTxt.Text = Convert.ToInt32(hexadecimalnum, 16).ToString();
+
+                // Convert the hexadecimal number to decimal number and assign it to the temp identifier
+                temp = Convert.ToInt32(hexadecimalnum, 16);
+
+                // Convert the decimal number that is assigned to the temp identifier to binary and update the binary text to display
+                // the binary equivalent to the hexadecimal number that the user inputted
+                BinaryTxt.Text = Convert.ToString(temp, 2);
+            } // if
+            else
+            {
+                // The user didn't enter a hexadecimal number so tell them to enter one
+                MessageBox.Show("Invalid - please enter a hexadecimal value in this box");
+
+                // Clear all of the text boxes
+                DecimalTxt.Clear();
+                BinaryTxt.Clear();
+                HexadecimalTxt.Clear();
+            } // else
+        } // HexadecimalBtn_Click
+
+    } // BaseConverter Form
+} // Base_Converter
